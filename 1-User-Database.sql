4bd7860c73c3bb5751736df47ad9b7bb75ba8f4f
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-04-30 14:51:46 +0100

    SQL syntax to create and insert values

---
+++ 1-User-Database.sql
@@ -0,0 +1,24 @@
+-- Creating a new database
+--CREATE DATABASE IF NOT EXISTS talent_hive;
+
+-- Creating custom ENUM type for Postgresql
+-- CREATE TYPE employment_status AS ENUM('employed', 'self-employed', 'unemployed');
+
+-- Create the table
+-- CREATE TABLE users (
+-- full_name VARCHAR(150),
+-- email VARCHAR(100),
+-- yearly_salary INT,
+-- current_status employment_status
+-- --current_status ENUM('employed', 'self-employed', 'unemployed')
+-- );
+
+-- Populating the table with data
+
+-- INSERT INTO users (full_name, email, yearly_salary, current_status) VALUES ('Aniebiet Afia', 'aniebietafia@gmail.com', 19000, 'unemployed');
+
+-- INSERT INTO users (full_name, email, yearly_salary, current_status) VALUES ('Martha Rittle', 'aniebietafia@gmail.com', 0, 'unemployed');
+
+-- INSERT INTO users (full_name, email, yearly_salary, current_status) VALUES ('James Ratt', 'aniebietafia@gmail.com', 15000, 'employed');
+
+SELECT * FROM users;
