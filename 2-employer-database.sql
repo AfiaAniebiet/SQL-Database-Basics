bb2e5ba7d157740445a0d52a9a382920f76d0fa8
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-04-30 14:52:23 +0100

    create employer database & insert values

---
+++ 2-employer-database.sql
@@ -0,0 +1,10 @@
+-- CREATE TABLE employers(
+--     company_name VARCHAR(120),
+--     company_address VARCHAR(120),
+--     yearly_revenue NUMERIC(13, 6),
+--     is_hiring BOOLEAN
+-- )
+
+-- INSERT INTO employers VALUES ('Talently Hive', '112 Boulevard Crescent, High Neptune, CA, USA', 1735106.570, TRUE)
+
+INSERT INTO employers VALUES ('Lodge Finder', '67 Engress Blue Stock, Platune, Ohio, Canada', 1473579.474, FALSE)
