db95d76fae7f942f8ca325b3d058562b6c781912
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-05-01 10:15:54 +0100

    create conversation table and insert values

--- 3-create-conversation.sql
+++ 3-create-conversation.sql
@@ -1,8 +1,10 @@
-CREATE TABLE conversation (
-    user_name VARCHAR(150),
-    employer_name VARCHAR(120),
-    message_content TEXT,
-    date_sent TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+-- CREATE TABLE conversations (
+--     user_name VARCHAR(150),
+--     employer_name VARCHAR(120),
+--     message_content TEXT,
+--     date_sent TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
 
--- INSERT INTO conversation VALUES ('Aniebiet Afia', 'Talently Hive', 'I just started learning SQL!')
+INSERT INTO conversations VALUES ('Aniebiet Afia', 'Talently Hive', 'I just started learning SQL!', date_sent);
+
+INSERT INTO conversations VALUES ('Ruth Jones', 'iTech', 'I just started learning SQL!', date_sent);
