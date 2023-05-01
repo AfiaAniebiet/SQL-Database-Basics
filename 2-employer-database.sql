d6b08e55fc3cb2abd37d51f86d85cded4da73fb0
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-05-01 10:15:49 +0100

    create employer database & insert values

--- 2-employer-database.sql
+++ 2-employer-database.sql
@@ -2,9 +2,13 @@
 --     company_name VARCHAR(120),
 --     company_address VARCHAR(120),
 --     yearly_revenue NUMERIC(13, 6),
---     is_hiring BOOLEAN
+--     is_hiring BOOLEAN DEFAULT FALSE
 -- )
 
--- INSERT INTO employers VALUES ('Talently Hive', '112 Boulevard Crescent, High Neptune, CA, USA', 1735106.570, TRUE)
+INSERT INTO employers VALUES ('Talently Hive', '112 Boulevard Crescent, High Neptune, CA, USA', 1735106.570, TRUE);
 
-INSERT INTO employers VALUES ('Lodge Finder', '67 Engress Blue Stock, Platune, Ohio, Canada', 1473579.474, FALSE)
+INSERT INTO employers VALUES ('Lodge Finder', '67 Engress Blue Stock, Platune, Ohio, Canada', 1473579.474, FALSE);
+
+INSERT INTO employers VALUES ('AltSchool Africa', '212 James Town Avenue, Cleverley Hills, Ramped Valley, Heavenly Town', 1269167.474, TRUE);
+
+INSERT INTO employers VALUES ('iTech', 'Arise Porch Estate, Platune, Ohio, Canada', 8243579.474, FALSE);
