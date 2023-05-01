3482f0aa8da6241d4ed9be0c72d956d22f40075a
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-05-01 10:15:43 +0100

    SQL syntax to create and insert values

--- 1-User-Database.sql
+++ 1-User-Database.sql
@@ -2,7 +2,7 @@
 --CREATE DATABASE IF NOT EXISTS talent_hive;
 
 -- Creating custom ENUM type for Postgresql
--- CREATE TYPE employment_status AS ENUM('employed', 'self-employed', 'unemployed');
+-- CREATE TYPE IF employment_status AS ENUM('employed', 'self-employed', 'unemployed');
 
 -- Create the table
 -- CREATE TABLE users (
@@ -10,15 +10,19 @@
 -- email VARCHAR(100),
 -- yearly_salary INT,
 -- current_status employment_status
--- --current_status ENUM('employed', 'self-employed', 'unemployed')
+-- -- current_status ENUM('employed', 'self-employed', 'unemployed')
 -- );
 
 -- Populating the table with data
 
--- INSERT INTO users (full_name, email, yearly_salary, current_status) VALUES ('Aniebiet Afia', 'aniebietafia@gmail.com', 19000, 'unemployed');
+INSERT INTO users (full_name, email, yearly_salary, current_status) VALUES ('Aniebiet Afia', 'aniebietafia@gmail.com', 19000, 'employed');
 
--- INSERT INTO users (full_name, email, yearly_salary, current_status) VALUES ('Martha Rittle', 'aniebietafia@gmail.com', 0, 'unemployed');
+INSERT INTO users (full_name, email, yearly_salary, current_status) VALUES ('Martha Rittle', 'aniebietafia@gmail.com', 0, 'unemployed');
 
--- INSERT INTO users (full_name, email, yearly_salary, current_status) VALUES ('James Ratt', 'aniebietafia@gmail.com', 15000, 'employed');
+INSERT INTO users (full_name, email, yearly_salary, current_status) VALUES ('James Ratt', 'aniebietafia@gmail.com', 15000, 'employed');
 
-SELECT * FROM users;
+INSERT INTO users (full_name, email, yearly_salary, current_status) VALUES ('Elizabeth Blantyre', 'blazzen@gmail.com', 0, 'unemployed');
+
+INSERT INTO users (full_name, email, yearly_salary, current_status) VALUES ('Ruth Jones', 'jones@gmail.com', 15000, 'self-employed');
+
+-- SELECT * FROM users;
